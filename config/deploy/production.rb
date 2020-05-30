178fc15ef057777d57be5b21ea8ce503513d988b
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-05-30 17:44:27 +0900

    deploy(basic認証)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 server '54.248.25.188', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 
 # server-based syntax
 # ======================
