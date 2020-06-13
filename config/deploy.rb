0f248202d93cd64ac6652ddc9d51b35cc61a5893
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-06-13 14:54:29 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :repo_url,  'git@github.com:fkoyo/freemarket_sample_71a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+#('config/secrets.yml')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
