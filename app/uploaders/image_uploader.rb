750459575b7eb37f2403cff31d7c13538f1b50e6
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-06-15 21:23:54 +0900

    index fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,13 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development?  #開発テスト環境
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else #それ以外（本番環境） 
-    storage :fog
-  end
+  # if Rails.env.development?  #開発テスト環境
+  #   storage :file
+  # elsif Rails.env.test?
+  #   storage :file
+  # else #それ以外（本番環境） 
+  #   storage :fog
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
