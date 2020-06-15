c7b890bb12ef09735f29ff6dfa36237d23756aaa
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-06-15 21:41:18 +0900

    index fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'homes/index'
   # root "purchase#index"
-  root "homes#index"
+  root to: 'homes#index'
   # resources :purchase, only: [:index]
   # root 'users#index'
 end
