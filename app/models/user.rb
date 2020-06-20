e1538a859601ac69b204e5f82f5a01307746cf03
shuta428 <shuhamu428@gmail.com>
2020-06-21 03:34:19 +0900

    setting-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :email, :last_name, :first_name, :last_name_fri, :first_name_fri, :birth_year, :birth_month, :birth_day, presence: true
+  has_one :address
 end
