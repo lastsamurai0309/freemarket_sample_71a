903cf662de7068913c45cb9302381e560be84bb4
shuta428 <shuhamu428@gmail.com>
2020-06-21 03:28:55 +0900

    delete-users-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200606063511) do
+ActiveRecord::Schema.define(version: 20200620182359) do
 
   create_table "purchase_histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
