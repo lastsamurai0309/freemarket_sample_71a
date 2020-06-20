225e9c77029b2d4a49970e26819037b5f0896e1b
shuta428 <shuhamu428@gmail.com>
2020-06-21 03:42:39 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,4 @@
 class Address < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :address_name, :address_furi, :post_num, :prefecture, :city, :address, :phone_number ,presence: true
 end
