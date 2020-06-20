1331607fc192e243c20b2a0b4c5054f59d9ab860
shuta428 <shuhamu428@gmail.com>
2020-06-21 03:32:30 +0900

    create-db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # get 'homes/index'
   # root "purchase#index"
   root to: 'homes#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200620182359) do
+ActiveRecord::Schema.define(version: 20200620183013) do
 
   create_table "purchase_histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                            null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "last_name",                           null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name_fri",                       null: false
+    t.string   "first_name_fri",                      null: false
+    t.integer  "birth_year",                          null: false
+    t.integer  "birth_month",                         null: false
+    t.integer  "birth_day",                           null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["birth_day"], name: "index_users_on_birth_day", unique: true, using: :btree
+    t.index ["birth_month"], name: "index_users_on_birth_month", unique: true, using: :btree
+    t.index ["birth_year"], name: "index_users_on_birth_year", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["first_name"], name: "index_users_on_first_name", unique: true, using: :btree
+    t.index ["first_name_fri"], name: "index_users_on_first_name_fri", unique: true, using: :btree
+    t.index ["last_name"], name: "index_users_on_last_name", unique: true, using: :btree
+    t.index ["last_name_fri"], name: "index_users_on_last_name_fri", unique: true, using: :btree
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
