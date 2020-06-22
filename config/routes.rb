ac43a957e346fcfb424a49c30ad972dc356c30fb
lastsamurai0309 <61785651+lastsamurai0309@users.noreply.github.com>
2020-06-22 20:51:45 +0900

    root修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  # get 'homes/index'
+  # root "purchase#index"
   root to: 'homes#index'
-
+  # resources :purchase, only: [:index]
+  # root 'users#index'
+  # root 'sell_item#index'
 end
 
