12ee8b3103ca23ade8ef517849f811de470b7c08
tomo-154 <tomo154yy@gmail.com>
2020-06-26 16:57:48 +0900

    purchase_history&migration

--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,5 +1,5 @@
 class PurchaseHistory < ApplicationRecord
 
-  belongs_to :user
+  # belongs_to :user
 
 end
--- db/migrate/20200606063511_create_purchase_histories.rb
+++ db/migrate/20200606063511_create_purchase_histories.rb
@@ -1,10 +1,10 @@
 class CreatePurchaseHistories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :purchase_histories do |t|
+  # def change
+  #   create_table :purchase_histories do |t|
 
-      t.references :user, foreign_key: true
+  #     t.references :user, foreign_key: true
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
