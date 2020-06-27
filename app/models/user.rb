8546a36e3e4bf3df0d5d539b0cdbcacf5343e348
shuta428 <shuhamu428@gmail.com>
2020-06-28 02:59:38 +0900

    モデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :birth_year, :birth_month, :birth_day, presence: true
+  validates :nickname, :last_name, :first_name, :last_name_fri, :first_name_fri, :birth_year, :birth_month, :birth_day, presence: true
   validates :password, presence: true, length: { minimum: 7 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name_fri, :first_name_fri, presence: true, format: { with: /\A[ぁ-んー－]+\z/}
   has_one :address
 
-  
 end
