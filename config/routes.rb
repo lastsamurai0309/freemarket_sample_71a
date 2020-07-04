5193df058b11f9fceb50f9ad117d92fdd0aac12a
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-07-04 10:14:47 +0900

    commit#3

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # get 'homes/index'
   # root "purchase#index"
   root to: "mypage#index"
-  get "mypage", to: 'mypage/card#index'
+  get "mypage/card", to: 'mypage#card'
   # root to: 'homes#index'
   # resources :purchase, only: [:index]
 
