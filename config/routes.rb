c64d98d707cda1e617fd22aa77f273d3373bfff2
tomo-154 <tomo154yy@gmail.com>
2020-07-04 11:32:03 +0900

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
 
-  # devise_for :users, controllers: {
-  #   registrations: 'users/registrations'
-  # }
-  # devise_scope :user do
-  #   get 'addresses', to: 'users/registrations#new_address'
-  #   post 'addresses', to: 'users/registrations#create_address'
-  # end
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   # get 'homes/index'
-  # root "purchase#index"
-  # root to: 'homes#index'
-  # resources :purchase, only: [:index]
-  # root 'users#index'
-  root 'mypage#index'
-
+  
+  root to: 'homes#index'  
+  resources :purchase, only: [:index]
+  # root 'users#index'  
+  
+  get 'mypage/index'
+  # resources :mypage
+  root "mypage#index"
+  get 'mypage/logout'
+  root "mypage#logout"
 end
 
