41bf0b1a9b07ae960c0a5a3a186f9acf4fb1b829
tomo-154 <tomo154yy@gmail.com>
2020-07-08 20:49:22 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 20200627033152) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "purchase_histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_purchase_histories_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
@@ -69,5 +62,4 @@ ActiveRecord::Schema.define(version: 20200627033152) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "purchase_histories", "users"
 end
