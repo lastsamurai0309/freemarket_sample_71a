05af9610cbe9107e9e6643306466f8bcec50adce
shuta428 <shuhamu428@gmail.com>
2020-07-10 20:06:22 +0900

    active-hashの設定

---
+++ app/models/condition.rb
@@ -0,0 +1,8 @@
+class Fee < ActiveHash::Base
+  self.data = [
+    {id: 0, name: '選択してください'},{id: 1, name: '新品'},{id: 2, name: '未使用'},{id: 3,name: '目立った傷なし'},{id: 4,name: '傷あり'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :products 
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,24 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :user
+  has_many :photos, dependent: :destroy
+  accepts_nested_attributes_for :photos, allow_destroy: true
+
+  belongs_to_active_hash :shippingday
+  belongs_to_active_hash :area
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :fee
+
+  with_options presence: true do
+  validates :name
+  validates :content
+  validates :price
+  validates :condition_id
+  validates :status
+  validates :fee_id
+  validates :area_id
+  validates :shippingday_id
+  validates :category_id
+  end
 end
