fbd928abc9ead7bc396052745fcade6feab96cf5
shuta428 <shuhamu428@gmail.com>
2020-07-10 19:37:41 +0900

    ビュー、コントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit]
+  before_action :set_params, only: :create
 
   def index
 
   end
 
   def new
-
+    @product = Product.new
+    @product.photos.new
   end
 
   def create
-
+    @product = Product.new(set_params)
+    if @product.valid?
+      @product.save
+    else
+      redirect_to new_product_path
+    end
   end
 
   def edit
@@ -26,5 +34,14 @@ class ProductsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_params
+    params.require(:product).permit(:name, :content, :category_id, :brand, :condition_id, :fee_id, :area_id, :shippingday_id, :price, photos_attributes: [:name, :_destroy, :id]).merge(user_id: current_user.id,status: 0)
   end
 end
