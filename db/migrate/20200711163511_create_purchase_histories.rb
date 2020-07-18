c007fa54bb1b9038f9cde189121105f317eede66
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-07-18 19:19:21 +0900

    本番環境反映のためのcommit

--- db/migrate/20200711163511_create_purchase_histories.rb
+++
@@ -1,9 +0,0 @@
-class CreatePurchaseHistories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :purchase_histories do |t|
-      t.references :user, foreign_key: true
-      t.references :product, foreign_key: true
-      t.timestamps
-    end
-  end
-end
