7bcc04c382913ada097b5248d0c65681fdccb35e
shuta428 <shuhamu428@gmail.com>
2020-07-24 23:28:19 +0900

    link-to-purchase-view

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action :set_items,only:[:index,:pay,:done]
   def index
-    @product = Product.find(1)
+    @product = Product.find(params[:product_id])
     @address = Address.find_by(user_id: current_user.id)
 
   end
@@ -9,5 +10,9 @@ class PurchasesController < ApplicationController
     @product = Product.find(1)
     @address = Address.find_by(user_id: current_user.id)
     @product.update(purchase_histories_id: current_user.id)
+  end
+
+  def set_items
+    @product = Product.find(params[:product_id])
   end
 end
