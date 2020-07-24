e51f9aeb28f04ddc4652443d2d783c752b504c4c
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-07-24 12:06:34 +0900

    scss

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,11 +1,11 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
-    console.log(2)
+    // console.log(2)
     Payjp.setPublicKey("pk_test_8c2bb5e7e6422b6570dc93a0");
 
     let btn = document.getElementById('payment_card_submit-button');
     btn.addEventListener("click", (e) => {
-      console.timeLog(3)
+      // console.timeLog(3)
       e.preventDefault();
       let card = {
         number: $("#card_number").val(),
