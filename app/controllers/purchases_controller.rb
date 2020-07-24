fee8be9b5aaa1a2f966f301352104bfb2a903777
tomo-154 <tomo154yy@gmail.com>
2020-07-24 11:32:42 +0900

    add-purchase-function

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,6 +8,6 @@ class PurchasesController < ApplicationController
   def done
     @product = Product.find(1)
     @address = Address.find_by(user_id: current_user.id)
-    # @product.update(purchase_histories_id: current_user.id)
+    @product.update(purchase_histories_id: current_user.id)
   end
 end
