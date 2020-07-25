da1847961e305583289a962511719ffb90e06252
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-07-25 16:45:01 +0900

    commit

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,15 @@
 class PurchasesController < ApplicationController
   require "payjp" 
-  before_action :purchas
-  # 購入と商品の違い？
+  # before_action :purchas
+  # # 購入と商品の違い？
   before_action :cache_card, only: [:index, :buy]
 
   require_relative './../commonclass/payjp.rb'
   before_action :set_items,only:[:index]
+  
   def index
     @product = Product.find(params[:product_id])
     @address = Address.find_by(user_id: current_user.id)
-  end
 
     if @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
@@ -41,58 +41,22 @@ class PurchasesController < ApplicationController
   end
 
   def buy
-
+# 　ここが、売り切れかどうか判断する記述です　ココから
     if @product.purchas.present? 
       bindng.pry
       redirect_to action: :index, alert:"この商品は売り切れています"
-  
-    elsif @card.blank?
-      redirect_to controller: :cards, action: :index, alert: "商品を購入するためには、クレジットカード登録が必要です。"
+#　ココまで  　以下コメントアウトは削除可能
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      #決まりの記載方法↓
-      Payjp::Charge.create(
-      amount: @product.price,
-      customer: @card.customer_id,
-      currency: 'jpy',
-      )
-        if @product.update(buyer_id: current_user.id)
-        
-          redirect_to root_path, notice: "購入しました。"
-        else
-          redirect_to action: 'index'
-        end
+      #売り切れてないときのコード記述＝大谷さんの内容？
     end
-  end
+  end 
 
   def done
     # @product = Product.find(1)
     @address = Address.find_by(user_id: current_user.id)
     @product.update(purchase_histories_id: current_user.id)
-  def new
-    @purchase = Purchase_history.new
-  end
-
-  def create
-    @purchase = Purchase_history.new(user_id:   current_user.id,
-                            product_id:  @product.id
-                           )
-    if @purchase.save
-      Payjp.create_charge_by_token(current_user.card.customer_id,
-                                    @product.price)
-      @product.update(status: 1)
-      redirect_to root_path, notice: "購入が完了しました"
-    else
-      render :new
-    end
-  end
-end
 
 private
-
-  def purchas
-    @product = Product.find(1)
-  end
 
   def cache_card
     @card = Card.find_by(user_id: current_user.id)
@@ -104,5 +68,3 @@ private
   def set_items
     @product = Product.find(params[:product_id])
   end
-
-
