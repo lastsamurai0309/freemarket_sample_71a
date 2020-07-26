905ad3c8b852a0b8188cbfca55cae48638c0c6d0
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-07-26 19:42:18 +0900

    commit

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,6 @@
 class PurchasesController < ApplicationController
   require "payjp" 
-  before_action :cache_card, only: [:index, :buy]
-  before_action :set_items,only:[:index]
+  before_action :set_item,only:[:index]
   before_action :set_card, :set_item
   
   def index
@@ -53,22 +52,41 @@ class PurchasesController < ApplicationController
   end
 
   def pay
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    Payjp::Charge.create(
-      :amount => @product.price, #支払金額を引っ張ってくる
-      :customer => @card.customer_id,  #顧客ID
-      :currency => 'jpy',              #日本円
-    )
-    redirect_to root_path #完了画面に移動
+    # if @product.present?
+    #   redirect_to product_path(@product.id), alert: "売り切れています。"
+    # else
+      # 同時に2人が同時に購入し、二重で購入処理がされることを防ぐための記述方法これらしい（５９行目）
+      @product.with_lock do
+      if current_user.card.present?
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        Payjp::Charge.create(
+          :amount => @product.price, #支払金額を引っ張ってくる
+          :customer => @card.customer_id,  #顧客ID
+          :currency => 'jpy',              #日本円
+        )
+        redirect_to root_path #完了画面に移動
+        # redirebt_to action: :done
+      else
+      # ログインユーザーがクレジットカード登録されていない場合(Checkout機能による処理を行います)
+      # APIの「Checkout」ライブラリによる決済処理の記述
+      # ※以下が難しい時は、マイページカード登録画面に飛ぶようにする？（本当ならよくないけど、今回はよしとする。？or登録してくださいのアラートのみ）
+      Payjp::Charge.create(
+      amount: @product.price,
+      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+      currency: 'jpy'
+      )
+    end
+    #ココにID書き換え処理
+      # @purchase = Purchase.create(buyer_id: current_user.id, product_id: params[:product_id])
+      params.require(:product).permit(:name, :content, :category_id, :brand, :condition_id, :fee_id, :area_id, :shippingday_id, :price, photos_attributes: [:name, :_destroy, :id]).merge(user_id: current_user.id,status: 2)
+    end
+      end
+    # end
   end
+
 
 
 private
-
-  def cache_card
-    @card = Card.find_by(user_id: current_user.id)
-  end
-
   def set_card
     @card = Card.find_by(user_id: current_user.id)
   end
@@ -76,4 +94,5 @@ private
   def set_item
     @product = Product.find(params[:product_id])
   end
+
 
