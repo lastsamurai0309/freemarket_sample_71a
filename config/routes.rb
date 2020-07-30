24f6fd41f1e1a972eca28e622938ca8bf8b7e9b3
lastsamurai0309 <61785651+lastsamurai0309@users.noreply.github.com>
2020-07-30 21:45:57 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # devise_for :users
   root to: 'products#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks'
+    # registrations: 'users/registrations'
+   }
+
 
   # devise_for :users, controllers: {
   #   registrations: 'users/registrations'
